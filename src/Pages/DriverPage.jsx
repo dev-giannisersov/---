94339cc89dddeb495d5a0bf1d17bb33e398e4580
Giannis Ersov <giannis.ersov@gmail.com>
2026-01-21 05:55:36 +0200

    Options for both types of users added (non-funtional, just skeleton)

--- src/Pages/DriverPage.jsx
+++ src/Pages/DriverPage.jsx
@@ -5,13 +5,38 @@ import { Link } from "react-router-dom";
 
 function DriverPage() {
     return (
-        <>
+        <div className='CENTER-OPTIONS'>
         <div>DRIVER</div>
 
-        <Link to="/">
-            <button>GO HOME</button>
+        <div className='Options-Driver'>
+    <div className='row-driver'>
+        <Link to="/driver/assistant" className="option-box">
+            Safety Assistant
         </Link>
-        </>
+
+        <Link to="/driver/climate" className="option-box">
+            Climate Control
+        </Link>
+    </div>
+
+    <div className='row-driver'>
+        <Link to="/driver/solar" className="option-box">
+            Solar Manager
+        </Link>
+
+        <Link to="/driver/robot" className="option-box">
+            Robot Vacuum
+        </Link>
+    </div>
+</div>
+
+        <Link to="/">
+            <button className='exit-button'>GO HOME</button>
+        </Link>
+        <div className='footer'>
+          ΕΡΓΑΣΙΑ ΑΛΛΗΛΕΠΙΔΡΑΣΙΑΣ ΑΝΘΡΩΠΟΥ-ΥΠΟΛΟΓΙΣΤΗ - ΕΡΣΩΒ ΙΩΑΝΝΗΣ - 2025
+        </div>
+        </div>
     )
 }
 
--- src/Pages/PassangerPage.jsx
+++ src/Pages/PassangerPage.jsx
@@ -5,13 +5,39 @@ import { Link } from "react-router-dom";
 
 function PassangerPage() {
     return (
-        <>
-        <div>Passanger</div>
+        <div className='CENTER-OPTIONS'>
+        <div>Passenger</div>
+
+        <div className='Options-Driver'>
+    <div className='row-driver'>
+        <Link to="/passenger/view" className="option-box">
+            Driver’s View
+        </Link>
+
+        <Link to="/passenger/map" className="option-box">
+            Smart Sightseeing
+        </Link>
+    </div>
+
+    <div className='row-driver'>
+        <Link to="/passenger/cafe" className="option-box">
+             Cafe Ordering
+        </Link>
+
+        <Link to="/passenger/walking" className="option-box">
+             Walking Mode
+        </Link>
+    </div>
+</div>
 
         <Link to="/">
-            <button>GO HOME</button>
+            <button className='exit-button'>GO HOME</button>
         </Link>
-        </>
+
+        <div className='footer'>
+          ΕΡΓΑΣΙΑ ΑΛΛΗΛΕΠΙΔΡΑΣΙΑΣ ΑΝΘΡΩΠΟΥ-ΥΠΟΛΟΓΙΣΤΗ - ΕΡΣΩΒ ΙΩΑΝΝΗΣ - 2025
+        </div>
+        </div>
     )
 }
 
