9f7efb25150dc3a96924274d8d9d9f8dd8579ed9
Giannis Ersov <giannis.ersov@gmail.com>
2026-01-24 12:33:37 +0200

    Added Walking Mode + Finished up SightSeeing

--- src/Pages/Sightseeing.jsx
+++ src/Pages/Sightseeing.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import PassangerButton from '../ExtraComponents/PassangerButton.jsx';
 
+import athens from '../Pictures/athens.webp';
+import panathinaiko from '../Pictures/panathinakio.jpg';
+import eris from '../Pictures/eris.jpg';  
+
 
 export class Sightseeing extends Component {
   render() {
     return (
       <div>
+        <div className='Sight-Main'>
+            <div className='Sight-Block'>
+               <img src={athens} alt="failed to load Pic_1" className='Pics_Sight'/>
+              <div className='Text_Describe'>Parthenon</div>
+            </div>
+            <div className='Sight-Block'>
+                <img src={panathinaiko} alt="failed to load Pic_2" className='Pics_Sight'/>
+                <div className='Text_Describe'>Panathinaiko Stadium</div>
+            </div>
+            <div className='Sight-Block'>
+                <img src={eris} alt="failed to load Pic_2" className='Pics_Sight'/>
+                <div className='Text_Describe'>Temple of Eris</div>
+            </div>
+        </div>
         <PassangerButton />
       </div>
     )
--- src/Pages/Walk.jsx
+++ src/Pages/Walk.jsx
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, useState, } from 'react'
 import PassangerButton from '../ExtraComponents/PassangerButton.jsx';
 
+import cartooi from '../Pictures/cartooi.avif';
 
-export class Walk extends Component {
-  render() {
+function Walk() {
+
+  const [isWalking, setIsWalking] = useState(false);
+
     return (
       <div>
-        <PassangerButton />
+        <div className='Map_hold'>
+           <img src={cartooi} alt="Failed to load the map" className='map'/>
+           <div 
+           className='Map_point'
+           style={{
+            animationName: isWalking ? 'Map_Point_Animation' : 'none'
+           }}
+           ></div>
+        </div>
+        <div className='Separator'>
+          <div 
+          className='Walk_Act'
+          onClick={() => {setIsWalking(true)}}
+          >Activate Navigation</div>
+            <PassangerButton />
+        </div>
+        
       </div>
     )
-  }
 }
 
 export default Walk
