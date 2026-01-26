5b08016be5d4d72cbb106efba06077dba5a9998b
Giannis Ersov <giannis.ersov@gmail.com>
2026-01-27 01:42:28 +0200

    Solar Manager Done!

--- src/Pages/Solar.jsx
+++ src/Pages/Solar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 import DriverButton from '../ExtraComponents/DriverButtoon.jsx';
 
+
 function Solar() {
+    const [Battery,setBattery] = useState(85);
+    const [isRoofOpen, setIsRoofOpen] = useState(false);
+
+    useEffect(() => {
+  const timer = setInterval(() => {
+    setBattery(prev => {
+      const change = isRoofOpen ? -1 : 1;
+      return Math.min(100, Math.max(0, prev + change));
+    });
+  }, 800);
+  return () => clearInterval(timer);
+}, [isRoofOpen]);
+
+
   return (
     <div>
+        <div className='Solar_Container'>
+            <div className='Solar_Left'
+            style={{backgroundColor : isRoofOpen ? 'green' : 'red'}}
+            onClick={() => {setIsRoofOpen(!isRoofOpen)}}>
+              <div className='Left_Title'>{isRoofOpen ? "ROOF OPEN" : "ROOF CLOSED"}</div>
+            </div>
+            <div className='Solar_Right'>
+                <h1 className='Solar_Right_title'>Solar Panel Battery</h1>
+                <div className='Battery_Status'>{Battery}%</div>
+            </div>
+        </div>
       <DriverButton/>
     </div>
   )
