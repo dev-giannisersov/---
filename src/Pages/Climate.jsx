5e4606b5dc140d5aae84382abf31de1a6ac6e6be
Giannis Ersov <giannis.ersov@gmail.com>
2026-01-27 05:14:13 +0200

    Vaccum Feature is DONE!

--- src/Pages/Climate.jsx
+++ src/Pages/Climate.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 
+import { useState,useEffect } from 'react';
+
 import DriverButton from '../ExtraComponents/DriverButtoon.jsx';
 
 function Climate() {
+  let [Temp, setTemp]= useState(21);
+  let [TargetTemp, SetTargetTemp]= useState(22);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(Temp < TargetTemp){
+        setTemp(Math.round((Temp + 0.1) * 10) / 10);
+        console.log(Temp);
+      }
+      else if(Temp > TargetTemp){
+        setTemp(Math.round((Temp - 0.1) * 10) / 10);
+        console.log(Temp);
+      }
+      else if(Temp === TargetTemp){
+        console.log(Temp);
+      }
+      else{
+        console.log("Mistake");
+      }
+
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [Temp, TargetTemp]);
+
   return (
     <div>
+      <div className='Current_Tempt'>
+        <div className='Top_Temp'>CURRENT TEMPERATURE</div>
+        <div className='Top_Temp'>{Temp} Celcius</div>
+      </div>
+      <div className='Target_Tempt'>
+        <div className='Top_Temp'>CURRENT TEMPERATURE</div>
+        <div className='Top_Temp'>{TargetTemp} Celcius</div>
+        <div className='ButtonTemp'
+          style={{right:'25px'}}
+          onClick={() => {SetTargetTemp(TargetTemp + 1)}}
+        >+</div>
+        <div className='ButtonTemp'
+          style={{left:'25px'}}
+
+          onClick={() => {SetTargetTemp(TargetTemp - 1)}}
+        >-</div>
+      </div>
       <DriverButton/>
     </div>
   )
--- src/Pages/Vacuum.jsx
+++ src/Pages/Vacuum.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 import DriverButton from '../ExtraComponents/DriverButtoon.jsx';
 
+
 function Vacuum() {
+
+  let [Value,setValue] = useState(0)
+  let [LoadBegan,setLoadBegan] = useState(false)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setValue(prev => {
+        const change = LoadBegan ? 1 : 0;
+        return Math.min(100, Math.max(0, prev + change));
+      });
+    }, 100);
+    return () => clearInterval(timer);
+  }, [LoadBegan]);
+
   return (
     <div>
+      <div className='Main_Vaccum_Box'>
+        <div className='Title_Vaccum'>SMART SPIDER CLEANER</div>
+        <div className='inner_Load'
+          style = {{animation : LoadBegan ? 'Load_Begin 11s ease-in-out forwards' : 'none'}}
+        ></div>
+        <div className='Load_Vaccum'>
+          {Value}%
+        </div>
+        <div className='Button_Vaccum'
+          onClick={() => {setLoadBegan(true)}}
+        >{LoadBegan ? "Vaccum ON" : "Vaccum OFF"}</div>
+        <div className='LostItems_Box'>
+          {Value === 100 ? 
+          <div>
+          <div className='LostItemPart'>LOST WALET FOUND</div> 
+          <div className='LostItemPart'>LOST BACKPACK FOUND</div> 
+          <div className='LostItemPart'>LOST PACKAGE FOUND</div>
+          <div className='LostItemPart'>LOST NECKLACE FOUND</div> 
+          </div>
+          : Value === 0 ?
+          <div className='NoItems'>PLEASE START THE VACCUM</div>
+          : 
+          <div className='NoItems'>VACCUM IS CURRENTLY ON....</div>
+          }
+        </div>
+      </div>
       <DriverButton/>
     </div>
   )
